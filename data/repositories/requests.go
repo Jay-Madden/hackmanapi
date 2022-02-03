fe922f3b213a6bdfe8a4844234fab3488ee1902d
Jay Madden <jaymaddencox@gmail.com>
2022-02-02 20:10:55 -0600

    Added logging to requests.go

--- data/repositories/requests.go
+++ data/repositories/requests.go
@@ -4,12 +4,15 @@ import (
 	"context"
 	"github.com/jackc/pgconn"
 	"hackmanapi/data"
+	"log"
 )
 
 func InsertRequest(db data.Database, userId int, word string, length string) (int, error) {
-
 	var comm pgconn.CommandTag
 	var err error
+
+	log.Printf("Inserting new Request with values (%v, %s, %s)\n", userId, word, length)
+
 	if length == "" {
 		comm, err = db.Pool.Exec(context.Background(),
 			`INSERT INTO "Requests" ("UserId", "ReturnedWord", "Length") VALUES ($1, $2, $3)`,
